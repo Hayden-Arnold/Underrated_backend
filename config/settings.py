1be62cc8165ca7043f705cb524a2e6791e4c664c
haydo <haydent2013is10@gmail.com>
2022-01-27 13:46:17 -0700

    more random stuff

--- config/settings.py
+++ config/settings.py
@@ -14,9 +14,15 @@ from pathlib import Path
 import os
 import dj_database_url
 
+from dotenv import load_dotenv
+load_dotenv()
+IS_PRODUCTION = os.getenv('PRODUCTION') == 'yes'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if IS_PRODUCTION:
+  BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+else:
+  BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,9 +84,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-"""
-DATABASES = {
+if IS_PRODUCTION:
+  DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+  }
+else:
+  DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'under',
@@ -88,10 +97,6 @@ DATABASES = {
         'PASSWORD': 'password',
         'HOST': 'localhost'
     }
-}
-"""
-DATABASES = {
-  'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
@@ -128,10 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
+if IS_PRODUCTION:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+  STATIC_URL = '/static/'
+  STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
+else:
+  STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
